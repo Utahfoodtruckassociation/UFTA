d685eb4bdbfd21f175df12a01ff52f95989e96e9
Jared Romney <jared.romney28@gmail.com>
2017-08-02 17:39:42 -0600

    Added devise

---
+++ app/controllers/concerns/default_page_content.rb
@@ -0,0 +1,13 @@
+module DefaultPageContent
+	extend ActiveSupport::Concern
+	
+
+	included do
+  before_filter :set_page_defaults
+	end
+
+  def set_page_defaults
+  	@page_title = "UFTA"
+  	@seo_keywords = "Food trucks"
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  devise_for :users
+
+  get "/trucks", to:'pages#trucks'
+
+  get "/about", to:'pages#about'
+
+  get "/contact", to:'pages#contact'
+
+  root to: 'pages#home'
+
 end
