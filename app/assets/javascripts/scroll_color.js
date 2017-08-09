6b3a94221899b4b07e708bd6ad6a5bbfef8adbca
Dallin Johnson <dallin.b.johnson@gmail.com>
2017-08-09 17:38:47 -0600

    fixed the have bar style so that it gose all the way accross the screen

--- app/assets/javascripts/scroll_color.js
+++ app/assets/javascripts/scroll_color.js
@@ -1,16 +1,15 @@
 $(document).ready(function(){       
-   var scroll_start = 0;
-   var startchange = $('#startchange');
-   var offset = startchange.offset();
-    if (startchange.length){
-
-     $(document).scroll(function() { 
-        scroll_start = $(this).scrollTop();
-        if(scroll_start > offset.top) {
-            $(".masthead").css('background-color', '#333');
-         } else {
-            $('.masthead').css('background-color', 'transparent');
-         }
-     });
-    }
+  var scroll_start = 0;
+  var startchange = $('#startchange');
+  var offset = startchange.offset();
+  if (startchange.length){
+   $(document).scroll(function() { 
+      scroll_start = $(this).scrollTop();
+      if(scroll_start > offset.top) {
+        $(".masthead").css('background-color', 'rgba(51, 51, 51, .7)');
+      } else {
+        $('.masthead').css('background-color', 'transparent');
+      }
+   });
+  }
 });
