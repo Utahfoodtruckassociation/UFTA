b19a969909bd5c1676031fbafe962969b81a04d8
Dallin Johnson <dallin.b.johnson@gmail.com>
2017-09-22 15:10:46 -0600

    Added content to the google map infowindow

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,7 @@
 class PagesController < ApplicationController
   def home
   	@trucks = Truck.order('created_at ASC')
+
   	@cal = GoogleCalendarAuth.new
 
     @glocation = []
@@ -12,6 +13,8 @@ class PagesController < ApplicationController
         @glocation[count] << event.summary
         @glocation[count] << event.start.date_time
         @glocation[count] << event.end.date_time
+        @glocation[count] << event.location
+        @glocation[count] << event.html_link
         count += 1
       end
   	end
@@ -20,7 +23,13 @@ class PagesController < ApplicationController
       loc.to_a
       marker.lat loc[0]
       marker.lng loc[1]
-      marker.infowindow "<img src='http://pocoinspired.com/t6/wp-content/uploads/2015/09/lunch-truck-it-favicon.jpg', width='50px' /> <br> <p>#{loc[2]}</p>"
+      marker.infowindow "<img src='http://pocoinspired.com/t6/wp-content/uploads/2015/09/lunch-truck-it-favicon.jpg', width='50px' /> <br> <a href='#{loc[6]}' target='_blank'>#{loc[2]}</a> <p>#{loc[3].strftime("%I:%M%p")} - #{loc[4].strftime("%I:%M%p")}</p> <a href='https://maps.google.com/maps?q=#{loc[5]}&hl=en' target='_blank'>#{loc[5]}</a>"
+    end
+    if @hash.empty?
+      @hash.push({
+        lat: request.location.latitude,
+        lng: request.location.longitude
+      })
     end
   end
 
