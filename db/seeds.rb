a9671bbcf0387ff11d706fc339a2813ed1031bd8
Dallin Johnson <dallin.b.johnson@gmail.com>
2017-12-10 11:58:51 -0700

    changed the docker file

--- db/seeds.rb
+++ db/seeds.rb
@@ -23,13 +23,14 @@
 
 # puts "2 Truck user created"
 
-User.find_or_create_by(
+mainAdminUser = User.new(
 	email: "admin@ufta.com",
 	password: "asdfasdf",
 	password_confirmation: "asdfasdf",
 	name: "Admin User",
 	roles: "admin"
 )
+mainAdminUser.save!
 
 puts "1 Admin user created"
 
