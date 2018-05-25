727b5c1c1f6476def1f4d8d7259e176f86e7e75c
Dallin B Johnson <dallin.b.johnson@gmail.com>
2018-05-24 22:38:50 -0600

    Added time zone functionality

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,10 @@ class ApplicationController < ActionController::Base
   include DeviseWhitelist
   include CurrentUserConcern
   include DefaultPageContent
+
+  around_action :user_time_zone, if: :current_user
+
+	def user_time_zone(&block)
+	  Time.use_zone(current_user.time_zone, &block)
+	end
 end
